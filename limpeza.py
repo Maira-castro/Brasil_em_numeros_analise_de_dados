f14ef9cf97b98f8c8f5821591d100c5876f7839b
Jefferson Rocha <jeffersonroch72@gmail.com>
2026-07-23 11:49:46 -0300

    Implementa limpeza e tratamento dos dados do IBGE

--- limpeza.py
+++ limpeza.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 def json_para_df(payload):
     series = payload[0]["resultados"][0]["series"]
     linhas = []
+
     for s in series:
 
         nome = s["localidade"]["nome"]
@@ -14,6 +16,7 @@ def json_para_df(payload):
             continue
 
         valor = float(valor)
+
         linhas.append({
             "uf_id": uf_id,
             "nome": nome,
@@ -25,13 +28,32 @@ def json_para_df(payload):
 
     df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
     df = df.dropna(subset=["valor"])
+
     return df
 
-def juntar_regiao(df, estados):
 
-    mapa_regiao = {
-        int(estado["id"]): estado["regiao"]["sigla"]
-        for estado in estados}
-        
-    df["regiao"] = df["uf_id"].map(mapa_regiao)
+def limpar_estados(estados):
+    linhas = []
+
+    for estado in estados:
+        linhas.append({
+            "uf_id": int(estado["id"]),
+            "sigla": estado["sigla"],
+            "nome_estado": estado["nome"],
+            "regiao": estado["regiao"]["sigla"]
+        })
+
+    df_estados = pd.DataFrame(linhas)
+
+    return df_estados
+
+
+def juntar_regiao(df, df_estados):
+
+    df = df.merge(
+        df_estados[["uf_id", "regiao"]],
+        on="uf_id",
+        how="left"
+    )
+
     return df

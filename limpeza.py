0c4819f3742aa4f05ad4fb40b9fac64031e2f7ab
Jefferson Rocha <jeffersonroch72@gmail.com>
2026-07-22 23:35:04 -0300

    Adiciona etapa de limpeza dos dados IBGE

---
+++ limpeza.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def json_para_df(payload):
+    series = payload[0]["resultados"][0]["series"]
+    linhas = []
+    for s in series:
+
+        nome = s["localidade"]["nome"]
+        uf_id = int(s["localidade"]["id"])
+
+        ano, valor = list(s["serie"].items())[-1]
+
+        if valor in ("...", "-"):
+            continue
+
+        valor = float(valor)
+        linhas.append({
+            "uf_id": uf_id,
+            "nome": nome,
+            "ano": ano,
+            "valor": valor
+        })
+
+    df = pd.DataFrame(linhas)
+
+    df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
+    df = df.dropna(subset=["valor"])
+    return df
+
+def juntar_regiao(df, estados):
+
+    mapa_regiao = {
+        int(estado["id"]): estado["regiao"]["sigla"]
+        for estado in estados}
+        
+    df["regiao"] = df["uf_id"].map(mapa_regiao)
+    return df

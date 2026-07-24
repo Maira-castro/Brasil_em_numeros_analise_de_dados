57beb65ecdaa86dde116c1946a826b1d35734ed0
Jefferson Rocha <jeffersonroch72@gmail.com>
2026-07-24 13:39:49 -0300

    Atualiza arquivo de limpeza dos dados do IBGE

--- limpeza.py
+++ limpeza.py
@@ -1,33 +1,63 @@
 import pandas as pd
 
 
-def transformar_json_em_df(payload):
-    series = payload[0]["resultados"][0]["series"]
-    linhas = []
+def limpar_populacao(dados):
 
-    for s in series:
+    registros = []
 
-        nome = s["localidade"]["nome"]
-        uf_id = int(s["localidade"]["id"])
+    series = dados[0]["resultados"][0]["series"]
 
-        ano, valor = list(s["serie"].items())[-1]
+    for estado in series:
 
-        if valor in ("...", "-"):
-            continue
+        nome_estado = estado["localidade"]["nome"]
 
-        valor = float(valor)
+        serie = estado["serie"]
 
-        linhas.append({
-            "uf_id": uf_id,
-            "nome": nome,
-            "ano": ano,
-            "valor": valor
-        })
+        for ano, valor in serie.items():
 
-    df = pd.DataFrame(linhas)
+            registros.append({
+                "uf_id": estado["localidade"]["id"],
+                "estado": nome_estado,
+                "ano": int(ano),
+                "populacao": int(valor)
+                })
 
-    df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
-    df = df.dropna(subset=["valor"])
+
+    df = pd.DataFrame(registros)
+
+
+    # ==========================
+    # LIMPEZA
+    # ==========================
+
+    # remover duplicados
+    df = df.drop_duplicates()
+
+
+    # ordenar
+    df = df.sort_values(
+        by=["estado", "ano"]
+    )
+
+
+    # verificar valores ausentes
+    df = df.dropna(
+        subset=[
+            "estado",
+            "ano",
+            "populacao"
+        ]
+    )
+
+
+    # garantir tipos
+    df["ano"] = df["ano"].astype(int)
+
+    df["populacao"] = (
+        df["populacao"]
+        .astype(int)
+    )
+
 
     return df
 
@@ -50,10 +80,13 @@ def limpar_estados(estados):
 
 def juntar_regiao(df, df_estados):
 
+    df["uf_id"] = df["uf_id"].astype(str)
+    df_estados["uf_id"] = df_estados["uf_id"].astype(str)
+
     df = df.merge(
         df_estados[["uf_id", "sigla", "regiao"]],
         on="uf_id",
         how="left"
     )
-
+    print(df)
     return df
